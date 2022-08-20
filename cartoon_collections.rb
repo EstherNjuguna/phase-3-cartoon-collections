f6368c86a8c212ef5144e1f15160980ed66df4b4
+ <+>
2022-08-20 20:07:35 +0300

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+
+  dwarves.map do |d|
+    puts "#{dwarves.index(d)+1}. #{d}"
+   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  capitalized = arr.map do |call|
+    call.capitalize + "!"
+  end
+  return capitalized
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  avail = arr.find do |word|
+    word.length > 4
+  end
+  if avail.class == String
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  finder = cheese_types.find do |type|
+    arr.include?(type)
+  end
+  finder
 end
+
